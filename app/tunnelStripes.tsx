0b35b49ac66ee6c13195900c8751b7ef3a3660f5
ComandoZeile <bit.konstrukteur@t-online.de>
2023-03-20 21:15:14 +0100

    Made useStyleProp actually return not undefined; Added responsive position change of stripes

--- app/tunnelStripes.tsx
+++ app/tunnelStripes.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useWindowSize } from "@/hooks/windowEvents";
+import { useWindowSize, WindowSize } from "@/hooks/windowEvents";
 import { useStyleProperty } from "@/hooks/documentEvents";
 
 import "@/styles/tunnelStripes.scss";
@@ -9,23 +9,30 @@ import "@/styles/tunnelStripes.scss";
 export default function TunnelStripes() {
     let stripes: Array<any> = [];
 
-    const { windowWidth, windowHeight } = useWindowSize();
+    const { windowWidth, windowHeight }: WindowSize = useWindowSize();
     console.log(windowWidth, " ------ ", windowHeight);
 
-    const circleRadius: number = Number(useStyleProperty("--circle-size")) / 2;
+    const circleRadius: number = parseFloat(useStyleProperty("--circle-size")) / 2;
 
-    const stripeCoordinates = (stripeAngle: number) => {
+    let stripeAngle: number = 0;
+    for (let index: number = 1; index <= 20; index++) {
+        console.log("circleRadius: " + circleRadius);
+        console.log("windowWidth: " + windowWidth);
         const posX = parseFloat(Math.cos(stripeAngle).toFixed(15)) * circleRadius + windowWidth / 2;
         const posY = parseFloat(Math.sin(stripeAngle).toFixed(15)) * circleRadius + windowHeight / 2;
-    }
 
-    let stripePos: object = {
-        "top": "0",
-        "left" : "0"
-    }
-    for (let index: number = 1; index <= 20; index++) {
+        console.log("posX: " + posX);
+        console.log("fixed x:" + posX.toFixed(1));
+        console.log("posY:" + posY);
+        console.log("fixed y: " + posY.toFixed(1));
 
+        let stripePos: object = {
+            "top": posY.toFixed(1) + "px",
+            "left" : posX.toFixed(1) + "px"
+        }
         stripes.push(<div className="stripe" style={stripePos} key={`stripe-${index}`}></div>);
+
+        stripeAngle += 18;
     }
 
     return (
--- hooks/documentEvents.ts
+++ hooks/documentEvents.ts
@@ -1,27 +1,25 @@
 "use client";
 
-import { useEffect } from "react";
 
+let stylePropValue: string | undefined;
 
-let stylePropValue: string;
-
-function useStyleProperty(propName: string, iniValue: string = ""): string | undefined {
-    useEffect(() => {
+function useStyleProperty(propName: string, iniValue: string = ""): any {
+    if (typeof window !== "undefined") {
         const root = document.documentElement;
 
         switch (iniValue) {
             case "":
-                console.log(`get prop: ${propName}`);
                 stylePropValue = root?.style.getPropertyValue(propName);
                 break;
             default:
-                console.log(`set prop name: ${propName} with value: ${iniValue}`);
                 root?.style.setProperty(propName, iniValue);
         }
-    });
+
+    }
 
     if (stylePropValue) return stylePropValue;
 }
+
 
 export {
     useStyleProperty
