3a242fdae060b9ee283e161a30ec5d9abdc40809
ComandoZeile <bit.konstrukteur@t-online.de>
2023-04-17 20:15:14 +0200

    Stripes are positioned approximately but not perfectly on circle; Refactored circleControl.tsx and circles.scss;

--- app/circleControl.tsx
+++ app/circleControl.tsx
@@ -16,22 +16,32 @@ export default function CircleControl() {
 
     const { windowWidth, windowHeight }: WindowSize = useWindowSize();
 
+    const CIRCLE_BORDER: number = 12;
+    // offset because of the wrapper object hasn't the full size
+    // wrapper doesn't take up the entire space (for 1920 the size is 1904 => 16px less => 8 on each side
+    const OFFSET_X: number = 8;
+    const OFFSET_Y: number = 14;
     const circleSize: number = 250;
+    let circles: Array<any> = [];
+    let newCircleSize: number = circleSize + CIRCLE_BORDER;
 
-    const [circlePos, setCirclePos] = useState<string>(windowHeight / 2 - circleSize / 2 - 14 + "px");
-    useStyleProperty("--circle-pos", circlePos);
+    const [circlePosX, setCirclePosX] = useState<string>(windowWidth / 2 - OFFSET_X + "px");
+    const [circlePosY, setCirclePosY] = useState<string>(windowHeight / 2 - circleSize / 2 - OFFSET_Y + "px");
+    useStyleProperty("--circle-pos-x", circlePosX);
+    useStyleProperty("--circle-pos-y", circlePosY);
 
     useEffect(() => {
-        setCirclePos(windowHeight / 2 - circleSize / 2 - 14 + "px");
+        setCirclePosX(windowWidth / 2 - OFFSET_X + "px");
+        setCirclePosY(windowHeight / 2 - circleSize / 2 - OFFSET_Y + "px");
     }, [windowWidth, windowHeight]);
 
-    let circles: Array<any> = [];
-    let newCircleSize: number = circleSize + 12;
     useStyleProperty("--circle-size", newCircleSize.toString());
-    for (let index:number = 1; windowWidth >= newCircleSize || windowHeight >= newCircleSize; index++) {
+    let index:number = 1;
+    for (index; windowWidth >= newCircleSize || windowHeight >= newCircleSize; index++) {
         circles = [...circles, <div className="circle" key={`circle-${index}`}></div>];
         newCircleSize += 160;
     }
+    circles = [...circles, <div className="circle" key={`circle-${index}`}></div>];
 
     useStyleProperty("--circle-count", circles.length.toString());
 
--- app/tunnelStripes.tsx
+++ app/tunnelStripes.tsx
@@ -8,18 +8,26 @@ import "@/styles/tunnelStripes.scss";
 
 export default function TunnelStripes() {
     let stripes: Array<any> = [];
+    // offset like in circleControl.tsx + stripe should stick half in border of circle => 3px
+    const OFFSET_X: number = 8 + 3;
+    const OFFSET_Y: number = 11 + 3;
 
     const { windowWidth, windowHeight }: WindowSize = useWindowSize();
     console.log(windowWidth, " ------ ", windowHeight);
 
     const circleRadius: number = parseFloat(useStyleProperty("--circle-size")) / 2;
 
+    const degreeToRadian: CallableFunction = (degree: number) => {
+        return degree * Math.PI / 180;
+    }
+
     let stripeAngle: number = 0;
-    for (let index: number = 1; index <= 20; index++) {
-        console.log("circleRadius: " + circleRadius);
-        console.log("windowWidth: " + windowWidth);
-        const posX = parseFloat(Math.cos(stripeAngle).toFixed(15)) * circleRadius + windowWidth / 2;
-        const posY = parseFloat(Math.sin(stripeAngle).toFixed(15)) * circleRadius + windowHeight / 2;
+    console.log("circleRadius: " + circleRadius);
+    console.log("windowWidth: " + windowWidth);
+    // 5 stripes in each 90-degree sector
+    for (let index: number = 1; index <= 6; index++) {
+        const posX = parseFloat(Math.cos(degreeToRadian(stripeAngle)).toFixed(15)) * circleRadius + windowWidth / 2 - OFFSET_X;
+        const posY = parseFloat(Math.sin(degreeToRadian(stripeAngle)).toFixed(15)) * circleRadius + windowHeight / 2 - OFFSET_Y;
 
         console.log("posX: " + posX);
         console.log("fixed x:" + posX.toFixed(1));
